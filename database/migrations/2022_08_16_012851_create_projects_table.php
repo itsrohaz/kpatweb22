03e3e31a092ac2b515cb77e1c28b44471d20638e
itsrohaz <110826618+itsrohaz@users.noreply.github.com>
2022-08-16 09:45:54 +0800

    update 3

---
+++ database/migrations/2022_08_16_012851_create_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('author' , 10);
+            $table->string('title' , 50);
+            $table->string('body');
+            $table->string('keyword');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
